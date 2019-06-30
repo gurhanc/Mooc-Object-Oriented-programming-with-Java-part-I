2c79eafb59bb502ab9f6a2e4f5b58651b20c1531
Gurhan Canan <gurhan.canan17@gmail.com>
2019-06-30 23:05:08 +0300

    "added new solution"

--- week3-049.LastCharacter/src/LastCharacter.java
+++ week3-049.LastCharacter/src/LastCharacter.java
@@ -6,5 +6,22 @@ public class LastCharacter {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
+        
+        System.out.println("Type your name: ");
+        
+        String name = reader.nextLine();
+        
+        System.out.println("Last character:"+lastCharacter(name));
+        
+        
     }
+    
+    
+    public static char lastCharacter(String text) {
+        
+        int length = text.length();
+        
+        return text.charAt(length-1);
+    }
+    
 }
