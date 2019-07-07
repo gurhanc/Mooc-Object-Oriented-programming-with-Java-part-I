1191ff46fce447e7dcd524c1e29fec11d8557173
Gurhan Canan <gurhan.canan17@gmail.com>
2019-07-08 00:58:51 +0300

    added new solutions

--- week4-075.DecreasingCounter/src/DecreasingCounter.java
+++ week4-075.DecreasingCounter/src/DecreasingCounter.java
@@ -1,8 +1,10 @@
 public class DecreasingCounter {
     private int value;  // instance variable that remembers the value of the counter
+    private int initial;
 
     public DecreasingCounter(int valueAtStart) {
         this.value = valueAtStart;
+        this.initial = valueAtStart;
     }
 
     public void printValue() {
@@ -11,8 +13,22 @@ public class DecreasingCounter {
     }
 
     public void decrease() {
-        // write here code to decrease counter value by one
+        
+        if (this.value > 0) {
+            this.value = this.value - 1;
+        }
+ 
     }
 
-    // and here the rest of the methods
+    public void reset() {
+        this.value = 0;
+    }
+    
+    
+    public void setInitial() {
+        
+        this.value = this.initial;
+        
+    }
+    
 }
