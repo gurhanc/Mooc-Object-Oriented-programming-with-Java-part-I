2906596ff8356e0a4c34d34f76453907c3f09ca4
Gurhan Canan <gurhan.canan17@gmail.com>
2019-07-08 22:32:16 +0300

    added new solutions

--- week4-076.Menu/src/Menu.java
+++ week4-076.Menu/src/Menu.java
@@ -9,5 +9,28 @@ public class Menu {
         this.meals = new ArrayList<String>();
     }
 
-    // add the methods here
+    
+    public void addMeal(String meal) {
+        
+        if (!this.meals.contains(meal)) {
+            
+            this.meals.add(meal);
+        }
+        
+    }
+    
+    public void printMeals() {
+        
+        int i;
+        
+        for (i=0;i<this.meals.size();i++) {
+            System.out.println(this.meals.get(i));
+        }
+        
+    }
+    
+     public void clearMenu() {
+         this.meals = new ArrayList<String>();
+     }
+    
 }
