2c0d0ada35549114880a2de2b152c8fda846af61
Gurhan Canan <gurhan.canan17@gmail.com>
2019-07-09 23:05:43 +0300

    added new solutions

--- week4-079.NumberStatistics/src/Main.java
+++ week4-079.NumberStatistics/src/Main.java
@@ -2,15 +2,38 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        
         Scanner reader = new Scanner(System.in);
-        // Write test code here
-        // Remember to remove all the extra code when doing assignments  79.3 and 79.4
-
-        // Define three NumberStatistics objects in your program:
-        // The first is used to track the sum of all given numbers.
-        // The second takes care of even numbers and the third the odd numbers.
-
-        // The tests does not work if you do not create the objects in the correct order
-
+        NumberStatistics total = new NumberStatistics();
+        NumberStatistics even = new NumberStatistics();
+        NumberStatistics odd = new NumberStatistics();
+        
+        
+        while (true) {
+            
+            System.out.println("Type numbers:");
+            
+            int number = Integer.parseInt(reader.nextLine());
+            
+            if (number == -1) {
+                break;
+            }
+            
+            total.addNumber(number);
+            
+            
+            if (number % 2 == 0) {
+                even.addNumber(number);
+            }
+            
+            if (number % 2 == 1) {
+                odd.addNumber(number);
+            }
+            
+        }
+        
+        System.out.println("sum: "+total.sum());
+        System.out.println("sum of even: "+even.sum());
+        System.out.println("sum of odd: "+odd.sum());
     }
 }
--- week4-079.NumberStatistics/src/NumberStatistics.java
+++ week4-079.NumberStatistics/src/NumberStatistics.java
@@ -1,3 +1,38 @@
 
 public class NumberStatistics {
+    private int amountOfNumbers = 0;
+    private int sum = 0;
+
+    public NumberStatistics() {
+        this.amountOfNumbers = amountOfNumbers;
+        this.sum = sum;
+    }
+
+    public void addNumber(int number) {
+        this.sum += number;
+        this.amountOfNumbers += 1;
+    }
+
+    public int amountOfNumbers() {
+        return this.amountOfNumbers;
+    }
+    
+    public int sum() {
+        return this.sum;  
+    }
+    
+    public double average() {
+        
+        if (this.amountOfNumbers > 0) {
+            return (double)this.sum / this.amountOfNumbers;
+        }
+        return 0;
+        
+        
+    }
+    
+    
+    
+    
 }
+    
