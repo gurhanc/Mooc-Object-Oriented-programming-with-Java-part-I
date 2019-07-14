63a30cc63531a6b942038f94eb6f0cc02c64e6fb
Gurhan Canan <gurhan.canan17@gmail.com>
2019-07-14 20:52:39 +0300

    added new solutions

--- week5-089.ClockObject/src/BoundedCounter.java
+++ week5-089.ClockObject/src/BoundedCounter.java
@@ -1,4 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
+/**
+ *
+ * @author gurhan
+ */
 public class BoundedCounter {
-    // copy here the class BoundedCounter from last weeks assignment 78 
+    
+    private int value;
+    private int upperLimit;
+    
+    public BoundedCounter(int upperLimit) {
+        
+        this.upperLimit = upperLimit;
+        this.value = 0;
+ 
+    }
+    
+    public void next() {
+        
+        this.value += 1;
+        
+        if (this.value > upperLimit) {
+            
+            this.value = 0;
+        }
+        
+    }
+    
+    public String toString() {
+        
+        if (this.value < 10) {
+            
+            return "0"+this.value;
+        }
+        else {
+            return ""+this.value;
+        }  
+    }
+    
+    
+    public int getValue() {
+        
+        return this.value;
+
+    }
+    
+    public void setValue(int parameter) {
+        
+        if ((0<= parameter) && (parameter <= upperLimit)) {
+            this.value = parameter;
+        }
+    }
+    
+
+    
 }
