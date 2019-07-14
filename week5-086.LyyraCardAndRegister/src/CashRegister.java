41373f299de446ef151edf3d812dc4e666bd414d
Gurhan Canan <gurhan.canan17@gmail.com>
2019-07-14 14:55:54 +0300

    added new solutions

--- week5-086.LyyraCardAndRegister/src/CashRegister.java
+++ week5-086.LyyraCardAndRegister/src/CashRegister.java
@@ -6,7 +6,13 @@ public class CashRegister {
     private int gourmetSold;
 
     public CashRegister() {
-        // at start the register has 1000 euros 
+        // at start the register has 1000 euros
+        this.cashInRegister = cashInRegister;
+        this.economicalSold = economicalSold;
+        this.gourmetSold = gourmetSold;
+        cashInRegister = 1000;
+        economicalSold = 0;
+        gourmetSold = 0;
     }
 
     public double payEconomical(double cashGiven) {
@@ -15,8 +21,13 @@ public class CashRegister {
         //    the price of lunch is added to register
         //    the amount of sold lunch is incremented by one
         //    method returns cashGiven - lunch price 
-        // if not enough money given, all is returned and nothing else happens        
-        return -1;
+        // if not enough money given, all is returned and nothing else happens
+        if (2.50 > cashGiven) {
+            return cashGiven;
+        }
+        cashInRegister += 2.50;
+        economicalSold += 1;
+        return cashGiven - 2.50;
     }
 
     public double payGourmet(double cashGiven) {
@@ -26,11 +37,42 @@ public class CashRegister {
         //    the amount of sold lunch is incremented by one
         //    method returns cashGiven - lunch price 
         // if not enough money given, all is returned and nothing else happens
-        return -1;
+        if (4.00 > cashGiven) {
+            return cashGiven;
+        }
+        cashInRegister += 4.00;
+        gourmetSold += 1;
+        return cashGiven - 4.00;  
     }
 
 
     public String toString() {
         return "money in register " + cashInRegister + " economical lunches sold: " + economicalSold + " gourmet lunches sold: " + gourmetSold;
     }
+    
+    public boolean payEconomical(LyyraCard card) {
+        if (card.balance() < 2.50) {
+            return false;
+        }
+        economicalSold += 1;
+        card.pay(2.50);
+        return true;
+    }
+    
+     public boolean payGourmet(LyyraCard card) {
+        if (card.balance() < 4.00) {
+            return false;
+        }
+        gourmetSold += 1;
+        card.pay(4.00);
+        return true;
+    }
+     
+    public void loadMoneyToCard(LyyraCard card, double sum) {
+        if (sum > 0) {
+            card.loadMoney(sum);
+            cashInRegister += sum;
+        }
+    }
+ 
 }
