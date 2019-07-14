b8bcec04f384fa978977aab1378d8388fdc31b49
Gurhan Canan <gurhan.canan17@gmail.com>
2019-07-14 17:25:48 +0300

    added new solutions

--- week5-088.Students/src/Main.java
+++ week5-088.Students/src/Main.java
@@ -3,8 +3,54 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+    
+    
 
     public static void main(String[] args) {
-        // write here the main program
+        
+        Scanner reader = new Scanner(System.in);
+        ArrayList<Student> list = new ArrayList<Student>();
+        
+        int i;
+        
+        
+        while (true) {
+            
+            System.out.println("name: ");
+            String name = reader.nextLine();
+            
+            if (name.equals("")) {
+                break;
+            }
+            System.out.println("studentnumber: ");
+            String studentNumber = reader.nextLine();
+            
+            Student student = new Student(name,studentNumber);
+            
+            list.add(student);
+            
+        }
+        
+        for (i=0;i<list.size();i++) {
+            System.out.println(list.get(i).toString());
+            
+        }
+        
+        System.out.println("Give search term: ");
+        
+        String searchTerm = reader.nextLine();
+        
+        System.out.println("Result:");
+        
+        for (i=0;i<list.size();i++) {
+            if (list.get(i).getName().contains(searchTerm)) {
+                System.out.println(list.get(i).toString());
+            }
+        }
+                
+        
+        
+        
+        
     }
 }
