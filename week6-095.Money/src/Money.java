982bdd40741270cc50edc904aadd42e6150562ae
Gurhan Canan <gurhan.canan17@gmail.com>
2019-07-17 22:39:41 +0300

    added new solutions

--- week6-095.Money/src/Money.java
+++ week6-095.Money/src/Money.java
@@ -1,3 +1,5 @@
+import java.lang.Math;
+
 
 public class Money {
 
@@ -31,6 +33,46 @@ public class Money {
         }
 
         return euros + "." + zero + cents + "e";
+    }
+    
+    public Money plus(Money added) {
+        Money sum = new Money(this.euros+added.euros,this.cents+added.cents);
+        return sum;
+    }
+    
+    public boolean less(Money compared) {
+        if (this.euros < compared.euros) {
+            return true;
+        }
+        else if (this.euros == compared.euros) {
+            if (this.cents < compared.cents) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public Money minus(Money decremented) {
+        
+        if (this.less(decremented) == true) {
+            return new Money(0,0);
+        }
+        
+        double total1 = (double)this.euros + (double)this.cents/100;
+        double total2 = (double)decremented.euros + (double)decremented.cents/100;
+        
+        double result = total1 - total2;
+        
+        System.out.println(result);
+        
+        System.out.println((int)Math.floor(result));
+        
+        System.out.println((int)(result-Math.floor(result)));
+        
+        
+        
+        return new Money((int)Math.floor(result),(int)(Math.round((result-Math.floor(result))*100)));
+        
     }
 
 }
